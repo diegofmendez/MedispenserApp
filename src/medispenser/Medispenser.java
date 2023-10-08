a33b26a3de500e145bbbfb48ad3be3a1f7ec6977
diegofmendez <dimendez@unal.edu.co>
2023-10-08 17:34:34 -0500

    Commit inicial

---
+++ src/medispenser/Medispenser.java
@@ -0,0 +1,25 @@
+
+package medispenser;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Medispenser {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Ventanaita");
+        window.setVisible(true);
+        window.setBounds(0,0,700,700);
+        window.setDefaultCloseOperation(3);
+         
+        JPanel panel=new JPanel();
+        panel.setBackground(Color.red);
+        window.add(panel);
+        
+        JButton boton = new JButton("Button");
+        panel.add(boton);
+    }
+}
