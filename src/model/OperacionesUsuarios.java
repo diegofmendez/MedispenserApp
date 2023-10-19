613a98f605d46d2b52dc5cb969e4740cc835e11c
diegofmendez <dimendez@unal.edu.co>
2023-10-19 07:32:57 -0500

    Archivos como jframes

--- src/model/OperacionesUsuarios.java
+++ src/model/OperacionesUsuarios.java
@@ -1,1 +1,51 @@
-Medisp
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class OperacionesUsuarios {
+    
+    final String tabla = "usuarios";
+    private Scanner scanner;
+            
+    public void registro(Connection conexion){
+        try {
+            PreparedStatement consulta;
+            consulta = conexion.prepareStatement("INSERT INTO "+ this.tabla +" (NAME, PASSWORD, TYPE, TELEFONO) VALUES (?,?,?,?)");
+            
+            scanner = new Scanner(System.in);
+
+            for (int i = 0; i < 4; i++) {
+                switch(i) {
+                    case 0:
+                        System.out.println("Ingresar tipo de usuario");
+                        break;
+                    case 1:
+                        System.out.println("Ingresar nombre completo");
+                        break;
+                    case 2:
+                        System.out.println("Ingresar contraseña");
+                        break;
+                    case 3:
+                        System.out.println("Ingresar número de telefono");
+                        break;
+                }
+                String texto = scanner.nextLine();
+                consulta.setString((i+1), texto);
+            }
+
+            consulta.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.out.println("Error en el registro: " + e.getMessage());
+             // Mensaje de error
+        } finally {
+            if (scanner != null) {
+                scanner.close(); // Cerrar el Scanner al finalizar
+            }
+        }
+    }
+}
+
--- src/modelView/medispenser.java
+++ src/modelView/medispenser.java
@@ -1,14 +1,12 @@
 
 package modelView;
 
-import model.OperacionesUsuarios;
 import view.Inicio;
 
 
 
 public class medispenser {
-    
-    private static java.sql.Connection con;
+
     
     public static void main(String[] args) {
         
@@ -17,7 +15,5 @@ public class medispenser {
         inici.setLocationRelativeTo(null);
         
 
-        OperacionesUsuarios opu = new OperacionesUsuarios();
-        opu.registro(con);
     }
 }
